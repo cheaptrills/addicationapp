53b0bc03baec76b7754ae7f030a1c99b217ad24d
cheaptrills <vincevanalphen@yahoo.com>
2020-04-18 16:02:56 +0200

    connection

--- addication/src/App.js
+++ addication/src/App.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import feathersjs from "@feathersjs/feathers";
+import Primus from "@feathersjs/primus-client";
+const socket = new Primus("http://localhost:3001");
+const f = feathersjs();
+f.configure(Primus(socket));
+//f.service("message").on("created",message => console.log(message));
+f.service("message").create({foo:"bar"});
+//import primus from "./primus";
+//let p = primus.connect("localhost:3001",{reconnect:{max:Infinity,min:500,retries:10}});
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Dit is React code
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.alphen.dev/addication"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Go to addication.com
         </a>
       </header>
     </div>
---
+++ addication/src/ProfielComponent.js
@@ -0,0 +1,538 @@
+/**
+ *
+ * This file was generated with Adobe XD React Exporter
+ * Exporter for Adobe XD is written by: Johannes Pichler <j.pichler@webpixels.at>
+ *
+ **/
+
+import React from "react";
+
+const ProfielComponent = () => (
+  <svg width={425} height={1239} viewBox="0 0 425 1239">
+    <defs>
+      <style>
+        {
+          ".a{clip-path:url(#ad);}.b{fill:url(#a);}.c{fill:rgba(255,255,255,0.9);}.d{fill:url(#f);}.e{fill:url(#g);}.f{fill:url(#h);}.g,.h,.k{fill:#30aebb;}.g{font-size:25px;}.g,.h,.k,.p{font-family:ArialRoundedMTBold, Arial Rounded MT Bold;}.h{font-size:18px;}.i{fill:#0ba5b0;}.j{fill:#7ac5d2;}.k{font-size:29px;}.l,.p{fill:#fff;}.m{fill:url(#z);}.n{fill:url(#aa);}.o{fill:url(#ab);}.p{font-size:15px;text-decoration:underline;}.q{filter:url(#w);}.r{filter:url(#u);}.s{filter:url(#s);}.t{filter:url(#q);}.u{filter:url(#o);}.v{filter:url(#m);}.w{filter:url(#k);}.x{filter:url(#i);}.y{filter:url(#d);}.z{filter:url(#b);}"
+        }
+      </style>
+      <linearGradient
+        id="a"
+        x1={0.5}
+        x2={1}
+        y2={1.068}
+        gradientUnits="objectBoundingBox"
+      >
+        <stop offset={0} stopColor="#30aebb" />
+        <stop offset={1} stopColor="#70bece" stopOpacity={0.749} />
+      </linearGradient>
+      <filter
+        id="b"
+        x={138}
+        y={58}
+        width={150}
+        height={140}
+        filterUnits="userSpaceOnUse"
+      >
+        <feOffset dy={3} input="SourceAlpha" />
+        <feGaussianBlur stdDeviation={3} result="c" />
+        <feFlood floodOpacity={0.161} />
+        <feComposite operator="in" in2="c" />
+        <feComposite in="SourceGraphic" />
+      </filter>
+      <filter
+        id="d"
+        x={38}
+        y={314}
+        width={349}
+        height={58}
+        filterUnits="userSpaceOnUse"
+      >
+        <feOffset dy={3} input="SourceAlpha" />
+        <feGaussianBlur stdDeviation={3} result="e" />
+        <feFlood floodOpacity={0.161} />
+        <feComposite operator="in" in2="e" />
+        <feComposite in="SourceGraphic" />
+      </filter>
+      <linearGradient
+        id="f"
+        x1={0.5}
+        x2={0.5}
+        y2={1}
+        gradientUnits="objectBoundingBox"
+      >
+        <stop offset={0} stopColor="#7fc7d3" />
+        <stop offset={1} stopColor="#3eb2bf" stopOpacity={0.929} />
+      </linearGradient>
+      <linearGradient
+        id="g"
+        x1={0.5}
+        y1={1.004}
+        x2={0.5}
+        y2={0.004}
+        gradientUnits="objectBoundingBox"
+      >
+        <stop offset={0} stopColor="#0ba5b0" />
+        <stop offset={0.984} stopColor="#6fbecd" />
+      </linearGradient>
+      <pattern
+        id="h"
+        preserveAspectRatio="none"
+        width="100%"
+        height="100%"
+        viewBox="0 0 290 492"
+      >
+        <image width={290} height={492} xlinkHref="ComponentTMP_0-image.png" />
+      </pattern>
+      <filter
+        id="i"
+        x={28}
+        y={410}
+        width={369}
+        height={85}
+        filterUnits="userSpaceOnUse"
+      >
+        <feOffset dy={3} input="SourceAlpha" />
+        <feGaussianBlur stdDeviation={3} result="j" />
+        <feFlood floodOpacity={0.161} />
+        <feComposite operator="in" in2="j" />
+        <feComposite in="SourceGraphic" />
+      </filter>
+      <filter
+        id="k"
+        x={28}
+        y={510}
+        width={369}
+        height={85}
+        filterUnits="userSpaceOnUse"
+      >
+        <feOffset dy={3} input="SourceAlpha" />
+        <feGaussianBlur stdDeviation={3} result="l" />
+        <feFlood floodOpacity={0.161} />
+        <feComposite operator="in" in2="l" />
+        <feComposite in="SourceGraphic" />
+      </filter>
+      <filter
+        id="m"
+        x={28}
+        y={610}
+        width={369}
+        height={85}
+        filterUnits="userSpaceOnUse"
+      >
+        <feOffset dy={3} input="SourceAlpha" />
+        <feGaussianBlur stdDeviation={3} result="n" />
+        <feFlood floodOpacity={0.161} />
+        <feComposite operator="in" in2="n" />
+        <feComposite in="SourceGraphic" />
+      </filter>
+      <filter
+        id="o"
+        x={28}
+        y={709}
+        width={369}
+        height={85}
+        filterUnits="userSpaceOnUse"
+      >
+        <feOffset dy={3} input="SourceAlpha" />
+        <feGaussianBlur stdDeviation={3} result="p" />
+        <feFlood floodOpacity={0.161} />
+        <feComposite operator="in" in2="p" />
+        <feComposite in="SourceGraphic" />
+      </filter>
+      <filter
+        id="q"
+        x={28}
+        y={907}
+        width={369}
+        height={85}
+        filterUnits="userSpaceOnUse"
+      >
+        <feOffset dy={3} input="SourceAlpha" />
+        <feGaussianBlur stdDeviation={3} result="r" />
+        <feFlood floodOpacity={0.161} />
+        <feComposite operator="in" in2="r" />
+        <feComposite in="SourceGraphic" />
+      </filter>
+      <filter
+        id="s"
+        x={28}
+        y={1025}
+        width={369}
+        height={85}
+        filterUnits="userSpaceOnUse"
+      >
+        <feOffset dy={3} input="SourceAlpha" />
+        <feGaussianBlur stdDeviation={3} result="t" />
+        <feFlood floodOpacity={0.161} />
+        <feComposite operator="in" in2="t" />
+        <feComposite in="SourceGraphic" />
+      </filter>
+      <filter
+        id="u"
+        x={28}
+        y={808}
+        width={369}
+        height={85}
+        filterUnits="userSpaceOnUse"
+      >
+        <feOffset dy={3} input="SourceAlpha" />
+        <feGaussianBlur stdDeviation={3} result="v" />
+        <feFlood floodOpacity={0.161} />
+        <feComposite operator="in" in2="v" />
+        <feComposite in="SourceGraphic" />
+      </filter>
+      <filter
+        id="w"
+        x={120}
+        y={229}
+        width={177}
+        height={75}
+        filterUnits="userSpaceOnUse"
+      >
+        <feOffset dy={3} input="SourceAlpha" />
+        <feGaussianBlur stdDeviation={3} result="x" />
+        <feFlood floodOpacity={0.161} />
+        <feComposite operator="in" in2="x" />
+        <feComposite in="SourceGraphic" />
+      </filter>
+      <pattern
+        id="z"
+        preserveAspectRatio="none"
+        width="100%"
+        height="100%"
+        viewBox="0 0 293 384"
+      >
+        <image width={293} height={384} xlinkHref="ComponentTMP_0-image2.png" />
+      </pattern>
+      <linearGradient
+        id="aa"
+        y1={0.5}
+        x2={1}
+        y2={0.5}
+        gradientUnits="objectBoundingBox"
+      >
+        <stop offset={0} stopColor="#04a4b0" />
+        <stop offset={0.984} stopColor="#70bdcc" />
+      </linearGradient>
+      <pattern
+        id="ab"
+        preserveAspectRatio="none"
+        width="100%"
+        height="100%"
+        viewBox="0 0 336 384"
+      >
+        <image width={336} height={384} xlinkHref="ComponentTMP_0-image3.png" />
+      </pattern>
+      <clipPath id="ad">
+        <rect width={425} height={1239} />
+      </clipPath>
+    </defs>
+    <g id="ac" className="a">
+      <rect className="l" width={425} height={1239} />
+      <rect
+        className="b"
+        width={425.5}
+        height={1241}
+        transform="translate(-0.5)"
+      />
+      <g transform="translate(147.154 64)">
+        <g className="z" transform="matrix(1, 0, 0, 1, -147.15, -64)">
+          <rect
+            className="c"
+            width={132}
+            height={122}
+            rx={22}
+            transform="translate(147 64)"
+          />
+        </g>
+      </g>
+      <g transform="translate(47 320.417)">
+        <g className="y" transform="matrix(1, 0, 0, 1, -47, -320.42)">
+          <rect
+            className="c"
+            width={331}
+            height={40}
+            rx={20}
+            transform="translate(47 320)"
+          />
+        </g>
+        <path
+          className="d"
+          d="M9.5,0h191c5.247,0,9.5,5.114,9.5,11.422s-4.253,11.422-9.5,11.422H9.5C4.253,22.844,0,17.73,0,11.422S4.253,0,9.5,0Z"
+          transform="translate(11 8.245)"
+        />
+      </g>
+      <path
+        className="e"
+        d="M56.788,40.233a33.543,33.543,0,0,0-12.641-7.417A17.692,17.692,0,0,0,52.5,17.99C52.5,8.07,43.87,0,33.265,0S14.034,8.07,14.034,17.99a17.692,17.692,0,0,0,8.35,14.825A33.543,33.543,0,0,0,9.743,40.233,29.94,29.94,0,0,0,0,62.237H5.2c0-14.478,12.591-26.256,28.068-26.256S61.333,47.759,61.333,62.237h5.2A29.94,29.94,0,0,0,56.788,40.233ZM19.232,17.99c0-7.239,6.3-13.128,14.034-13.128S47.3,10.751,47.3,17.99,41,31.118,33.265,31.118,19.232,25.229,19.232,17.99Z"
+        transform="translate(179.41 87.413)"
+      />
+      <rect className="f" width={25} height={41} transform="translate(29 36)" />
+      <g transform="translate(37 416.416)">
+        <g transform="translate(0)">
+          <g className="x" transform="matrix(1, 0, 0, 1, -37, -416.42)">
+            <rect
+              className="c"
+              width={351}
+              height={67}
+              rx={22}
+              transform="translate(37 416)"
+            />
+          </g>
+        </g>
+      </g>
+      <g transform="translate(37 515.994)">
+        <g transform="translate(0)">
+          <g className="w" transform="matrix(1, 0, 0, 1, -37, -515.99)">
+            <rect
+              className="c"
+              width={351}
+              height={67}
+              rx={22}
+              transform="translate(37 516)"
+            />
+          </g>
+        </g>
+      </g>
+      <g transform="translate(37 615.572)">
+        <g transform="translate(0)">
+          <g className="v" transform="matrix(1, 0, 0, 1, -37, -615.57)">
+            <rect
+              className="c"
+              width={351}
+              height={67}
+              rx={22}
+              transform="translate(37 616)"
+            />
+          </g>
+        </g>
+      </g>
+      <g transform="translate(37 715.15)">
+        <g transform="translate(0)">
+          <g className="u" transform="matrix(1, 0, 0, 1, -37, -715.15)">
+            <rect
+              className="c"
+              width={351}
+              height={67}
+              rx={22}
+              transform="translate(37 715)"
+            />
+          </g>
+        </g>
+      </g>
+      <g transform="translate(37 913.174)">
+        <g transform="translate(0)">
+          <g className="t" transform="matrix(1, 0, 0, 1, -37, -913.17)">
+            <rect
+              className="c"
+              width={351}
+              height={67}
+              rx={22}
+              transform="translate(37 913)"
+            />
+          </g>
+        </g>
+      </g>
+      <g transform="translate(37 1031.5)">
+        <g transform="translate(0)">
+          <g className="s" transform="matrix(1, 0, 0, 1, -37, -1031.5)">
+            <rect
+              className="c"
+              width={351}
+              height={67}
+              rx={22}
+              transform="translate(37 1031)"
+            />
+          </g>
+        </g>
+      </g>
+      <g transform="translate(37 813.596)">
+        <g transform="translate(0)">
+          <g className="r" transform="matrix(1, 0, 0, 1, -37, -813.6)">
+            <rect
+              className="c"
+              width={351}
+              height={67}
+              rx={22}
+              transform="translate(37 814)"
+            />
+          </g>
+        </g>
+      </g>
+      <text className="g" transform="translate(101 434)">
+        <tspan x={58.356} y={24}>
+          {"Jan"}
+        </tspan>
+      </text>
+      <text className="g" transform="translate(99 533)">
+        <tspan x={36.604} y={24}>
+          {"Jansen"}
+        </tspan>
+      </text>
+      <text className="h" transform="translate(159 655)">
+        <tspan x={-104.73} y={0}>
+          {"janjansen@hotmail.com"}
+        </tspan>
+      </text>
+      <text className="h" transform="translate(213 952)">
+        <tspan x={-114.627} y={0}>
+          {"RESET JE VOORUITGANG"}
+        </tspan>
+      </text>
+      <text className="h" transform="translate(213 1071)">
+        <tspan x={-54.374} y={0}>
+          {"UITLOGGEN"}
+        </tspan>
+      </text>
+      <text className="h" transform="translate(213 852)">
+        <tspan x={-107.543} y={0}>
+          {"VERANDER VAN DRUGS"}
+        </tspan>
+      </text>
+      <path
+        className="i"
+        d="M25.732,11.861l2.008,2.005L9.986,31.62,0,33.632l2.017-9.987L19.765,5.893,21.771,7.9,4.6,25.028,3.58,30.05,8.6,29.037ZM25.654,0,21.709,3.948l7.975,7.976,3.948-3.953L26.524.869,25.654,0ZM8.392,26.23,24.246,10.373l-.988-.991L7.4,25.237l.988.994Z"
+        transform="translate(318.731 434.074)"
+      />
+      <path
+        className="i"
+        d="M25.732,11.861l2.008,2.005L9.986,31.62,0,33.632l2.017-9.987L19.765,5.893,21.771,7.9,4.6,25.028,3.58,30.05,8.6,29.037ZM25.654,0,21.709,3.948l7.975,7.976,3.948-3.953L25.654,0ZM8.392,26.23,24.246,10.373l-.988-.991L7.4,25.237l.988.994Z"
+        transform="translate(313 531.037)"
+      />
+      <path
+        className="i"
+        d="M25.732,11.861l2.008,2.005L9.986,31.62,0,33.632l2.017-9.987L19.765,5.893,21.771,7.9,4.6,25.028,3.58,30.05,8.6,29.037ZM25.654,0,21.709,3.948l7.975,7.976,3.948-3.953L25.654,0ZM8.392,26.23,24.246,10.373l-.988-.991L7.4,25.237l.988.994Z"
+        transform="translate(313 627.999)"
+      />
+      <path
+        className="i"
+        d="M25.732,11.861l2.008,2.005L9.986,31.62,0,33.632l2.017-9.987L19.765,5.893,21.771,7.9,4.6,25.028,3.58,30.05,8.6,29.037ZM25.654,0,21.709,3.948l7.975,7.976,3.948-3.953L25.654,0ZM8.392,26.23,24.246,10.373l-.988-.991L7.4,25.237l.988.994Z"
+        transform="translate(318.731 728.343)"
+      />
+      <ellipse
+        className="j"
+        cx={7}
+        cy={7.5}
+        rx={7}
+        ry={7.5}
+        transform="translate(93 740)"
+      />
+      <ellipse
+        className="j"
+        cx={7}
+        cy={7.5}
+        rx={7}
+        ry={7.5}
+        transform="translate(120 740)"
+      />
+      <ellipse
+        className="j"
+        cx={7}
+        cy={7.5}
+        rx={7}
+        ry={7.5}
+        transform="translate(146 740)"
+      />
+      <ellipse
+        className="j"
+        cx={7}
+        cy={7.5}
+        rx={7}
+        ry={7.5}
+        transform="translate(172 740)"
+      />
+      <ellipse
+        className="j"
+        cx={7}
+        cy={7.5}
+        rx={7}
+        ry={7.5}
+        transform="translate(198 740)"
+      />
+      <ellipse
+        className="j"
+        cx={7}
+        cy={7.5}
+        rx={7}
+        ry={7.5}
+        transform="translate(224 740)"
+      />
+      <ellipse
+        className="j"
+        cx={7}
+        cy={7.5}
+        rx={7}
+        ry={7.5}
+        transform="translate(250 740)"
+      />
+      <ellipse
+        className="j"
+        cx={7}
+        cy={7.5}
+        rx={7}
+        ry={7.5}
+        transform="translate(277 740)"
+      />
+      <ellipse
+        className="j"
+        cx={7}
+        cy={7.5}
+        rx={7}
+        ry={7.5}
+        transform="translate(59 740)"
+      />
+      <g transform="translate(129.365 235.104)">
+        <g className="q" transform="matrix(1, 0, 0, 1, -129.36, -235.1)">
+          <rect
+            className="c"
+            width={159}
+            height={57}
+            rx={22}
+            transform="translate(129 235)"
+          />
+        </g>
+      </g>
+      <text className="k" transform="translate(208 275)">
+        <tspan x={-44.64} y={0}>
+          {"level 3"}
+        </tspan>
+      </text>
+      <rect
+        className="l"
+        width={393}
+        height={68}
+        rx={34}
+        transform="translate(16 1157)"
+      />
+      <path
+        className="e"
+        d="M33.331,25.243a19.45,19.45,0,0,0-7.419-4.654,11.288,11.288,0,1,0-12.773,0A19.556,19.556,0,0,0,0,39.05H3.051A16.474,16.474,0,1,1,36,39.05H39.05A19.4,19.4,0,0,0,33.331,25.243ZM11.288,11.288a8.237,8.237,0,1,1,8.237,8.237A8.246,8.246,0,0,1,11.288,11.288Z"
+        transform="translate(339.351 1170.1)"
+      />
+      <rect
+        className="m"
+        width={32}
+        height={41}
+        transform="translate(248 1170)"
+      />
+      <path
+        className="n"
+        d="M34.822,51.725h-10.1a1.208,1.208,0,0,1-1.208-1.208V40.3H17.573V50.517a1.208,1.208,0,0,1-1.208,1.208H6.261a1.208,1.208,0,0,1-1.208-1.208V33.688H2.109A2.107,2.107,0,0,1,.69,30.024l18.433-16.8a2.1,2.1,0,0,1,2.838,0l4.191,3.819v-.672a1.208,1.208,0,0,1,1.208-1.208h8.354a1.208,1.208,0,0,1,1.208,1.208V26.862l3.471,3.162a2.107,2.107,0,0,1-1.419,3.664H36.03V50.517A1.208,1.208,0,0,1,34.822,51.725Zm-8.895-2.416h7.687V32.48a1.208,1.208,0,0,1,1.208-1.208h3.353L34.9,28.289a1.208,1.208,0,0,1-.394-.893V17.584H28.569v2.2a1.208,1.208,0,0,1-2.022.893l-6-5.472L2.908,31.272H6.261A1.208,1.208,0,0,1,7.469,32.48V49.309h7.687V39.091a1.208,1.208,0,0,1,1.208-1.208h8.354a1.208,1.208,0,0,1,1.208,1.208V49.309Z"
+        transform="translate(40.916 1157.424)"
+      />
+      <rect
+        className="o"
+        width={36}
+        height={41}
+        transform="translate(149 1170)"
+      />
+      <text className="p" transform="translate(213 1133)">
+        <tspan x={-74.154} y={0}>
+          {"verwijder je account"}
+        </tspan>
+      </text>
+    </g>
+  </svg>
+);
+
+export default ProfielComponent;
