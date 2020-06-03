b1d16115a8ba3976009096b8768e7b9a0ba69c43
KlowiChloe <chloe.ryckx@telenet.be>
2020-06-03 13:03:48 +0200

    login aanpassing svg links

--- addication/src/routes/Login.js
+++ addication/src/routes/Login.js
@@ -7,16 +7,20 @@ function Login() {
   return (
     
     <div className="base-container">
+      <header>
+       <img src="../SVG/logo.svg" class="logo"/>
+       
+   </header>
         <div className="header">Login</div>
         <div className="content">
           <div className="form" method="post">
             <div className="form__field">
-            <img src="profiel.svg" class="icon" id="profileicon"> </img>
+            <img src="../SVG/profiel.svg" class="icon" id="profileicon"> </img>
             <input type="text" class="form-control" name="name" id="naam" placeholder="Gebruikersnaam"> </input>
               
             </div>
             <div className="form__field">
-            <img src="password.svg" class="icon" > </img>
+            <img src="../SVG/password.svg" class="icon" > </img>
               <input type="password" class="form-control" name="password" id="password" placeholder="Wachtwoord"></input>
             </div>
           
@@ -25,7 +29,7 @@ function Login() {
 					<input type="submit" value="maak account" class="btn"> </input>
           </div>
 				</div>
-        <a class="link-account" href="register.html">Heb je nog geen account?</a>
+        <a class="link-account" href="register.js">Heb je nog geen account?</a>
         </div>
       </div>
 
