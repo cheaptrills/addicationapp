ea0ca66222aa369161175ec3f38ebbbafdf1b368
KlowiChloe <chloe.ryckx@telenet.be>
2020-06-04 14:08:12 +0200

    html opdrachten import css

---
+++ addication/src/routes/Level.js
@@ -0,0 +1,31 @@
+import React from 'react';
+//import './App.css';
+//import './chloestyle.css';
+
+function Level() {
+
+  return (
+    <div className="base-container">
+        <div class="levelsheader"> 
+         <img src="SVG/level2.svg" class="lev"/> 
+         <img src="SVG/level3.svg" class="lev"/>
+         <img src="SVG/level4.svg" class="lev"/>
+         <img src="SVG/level5.svg" class="lev"/>
+         <img src="SVG/level6.svg" class="lev"/>
+         <img src="SVG/level7.svg" class="lev"/>
+         <img src="SVG/level8.svg" class="lev"/>
+         
+     </div>
+     <br/>
+      <div class="currentlevel">
+      <img src="SVG/level1.svg" class="level"/>
+       <img src="SVG/level-balk.svg" class="levelbalk"/>
+       <img src="SVG/level-aanduiding.svg" class="levelaanduid"/>
+       <img src="SVG/level-aanduiding.svg" class="levelaanduid1"/>
+       
+       </div>
+      </div>
+    );
+}
+
+export default Level;
---
+++ addication/src/routes/Opdrachten.js
@@ -0,0 +1,63 @@
+import React from 'react';
+//import './App.css';
+//import './chloestyle.css';
+
+function Opdrachten() {
+
+  return (
+    <div className="base-container">
+         <header>
+    <div class="start"> 
+       <a href="dashboard.html"> 
+        <img src="SVG/back-button.svg" alt="terugknop" class="terugknop"/>
+        </a>
+        <p class="title"> OPDRACHTEN </p>
+    
+    </div>
+    
+     <br>
+      <div class="currentlevel">
+      <img src="SVG/level1.svg" class="level"/>
+       <img src="SVG/level-balk.svg" class="levelbalk"/>
+       <img src="SVG/level-aanduiding.svg" class="levelaanduid"/>
+       <img src="SVG/level-aanduiding.svg" class="levelaanduid1"/>
+       
+       </div>
+   </header>
+       <div class="maindash"> 
+       <a href="opdracht-keuze.html">
+     <button class="dash" id="opdrachten"> 
+     <div class="inhoud">
+      
+      <p> KIES OPDRACHT</p>
+      </div>
+     </button>
+      </a>
+      
+      <a href="medailles-opdracht.html">
+      <button class="dash" id="dagboek"> 
+      <div class="inhoud">
+     
+      <p> MEDAILLES</p>
+          </div>
+     </button>
+      </a> 
+      
+      <a href="opdracht-hulp.html"> 
+       <button class="dash" id="noodknop"> 
+       <div class="inhoud">
+      <img src="SVG/hulp-icon.svg" alt="hulp" class="hulpicon"/>
+      <p> HULP</p>
+           </div>
+     </button>
+       </a>
+       
+      
+ 
+   </div>
+    
+      </div>
+    );
+}
+
+export default Opdrachten;
