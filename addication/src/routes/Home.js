a1b099b400c6d95f0cf9a7b3228b1d24623d55c6
cheaptrills <vincevanalphen@yahoo.com>
2020-06-05 13:12:20 +0200

    added home to app.js

--- addication/src/routes/Home.js
+++ addication/src/routes/Home.js
@@ -7,24 +7,23 @@ function Home() {
   return (
     <div className="base-container">
         <header>
-     <div class="levelsheader"> 
-         <img src="SVG/level2.svg" class="lev"/> 
-         <img src="SVG/level3.svg" class="lev"/>
-         <img src="SVG/level4.svg" class="lev"/>
-         <img src="SVG/level5.svg" class="lev"/>
-         <img src="SVG/level6.svg" class="lev"/>
-         <img src="SVG/level7.svg" class="lev"/>
-         <img src="SVG/level8.svg" class="lev"/>
-     </div>
-     <br>
-      <div class="currentlevel">
-      <img src="SVG/level1.svg" class="level"/>
-       <img src="SVG/level-balk.svg" class="levelbalk"/>
-       <img src="SVG/level-aanduiding.svg" class="levelaanduid"/>
-       <img src="SVG/level-aanduiding.svg" class="levelaanduid1"/>
-       
-       </div>
-   </header>
+          <div class="levelsheader"> 
+              <img src="SVG/level2.svg" class="lev"/> 
+              <img src="SVG/level3.svg" class="lev"/>
+              <img src="SVG/level4.svg" class="lev"/>
+              <img src="SVG/level5.svg" class="lev"/>
+              <img src="SVG/level6.svg" class="lev"/>
+              <img src="SVG/level7.svg" class="lev"/>
+              <img src="SVG/level8.svg" class="lev"/>
+          </div>
+          <br />
+          <div class="currentlevel">
+            <img src="SVG/level1.svg" class="level"/>
+            <img src="SVG/level-balk.svg" class="levelbalk"/>
+            <img src="SVG/level-aanduiding.svg" class="levelaanduid"/>
+            <img src="SVG/level-aanduiding.svg" class="levelaanduid1"/>
+          </div>
+        </header>
        <div class="maindash"> 
        <a href="opdrachten.html">
      <button class="dash" id="opdrachten"> 
