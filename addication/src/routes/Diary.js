22845a39fc3028262f078b3110b36aac643a56a7
KlowiChloe <chloe.ryckx@telenet.be>
2020-06-05 14:48:24 +0200

    html dagboek

--- addication/src/routes/Diary.js
+++ addication/src/routes/Diary.js
@@ -6,10 +6,48 @@ function Diary() {
 
   return (
 
-    <div>
-        <p>
-        </p>
+    <header>
+    <div class="start"> 
+       <a href="dashboard.html"> 
+        <img src="SVG/back-button.svg" alt="terugknop" class="terugknop"/>
+        </a>
     </div>
+     <br/>
+      <div class="currentlevel" /*hier komt include van level */>
+      <img src="SVG/level1.svg" class="level"/>
+       <img src="SVG/level-balk.svg" class="levelbalk"/>
+       <img src="SVG/level-aanduiding.svg" class="levelaanduid"/>
+       <img src="SVG/level-aanduiding.svg" class="levelaanduid1"/>
+       </div>
+   </header>
+       <div class="maindash"> 
+       <a href="dagboek-leesdag.html">
+     <button class="dash" id="opdrachten"> 
+     <div class="inhoud">
+      
+      <p class="dag"> DAG 1</p>
+      </div>
+     </button>
+      </a>
+      
+      <a href="dagboek-leesdag.html">
+      <button class="dash" id="dagboek"> 
+      <div class="inhoud">
+     
+      <p class="dag"> DAG 2</p>
+          </div>
+     </button>
+      </a> 
+      
+      <a href="dagboek-leesdag.html">
+      <button class="dash" id="dagboek"> 
+      <div class="inhoud">
+     
+      <p class="dag"> DAG 3</p>
+          </div>
+     </button>
+      </a> 
+   </div>
   );
 
     
