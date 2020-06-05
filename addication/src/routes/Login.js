5b0585362772688b7540260e4b900084c9f5a6a1
cheaptrills <vincevanalphen@yahoo.com>
2020-06-05 10:26:07 +0200

    login react klaar gemaakt
    SVG map zit hier nog NIET in dus geeft nog fouten op de login screen

--- addication/src/routes/Login.js
+++ addication/src/routes/Login.js
@@ -2,40 +2,38 @@ import React from 'react';
 //import './App.css';
 //import './stylechloe.css';
 
+import logosvg from "../SVG/logo.svg";
+import profielsvg from "../SVG/profiel.svg";
+import passwordsvg from "../SVG/password.svg";
+
 function Login() {
 
   return (
-    
-    <div className="base-container">
-      <header>
-       <img src="../SVG/logo.svg" class="logo"/>
-       
-   </header>
+    <div className="App"> 
+      <div className="base-container">
+        <header>
+          <img src={logosvg} class="logo"/> 
+        </header>
         <div className="header">Login</div>
-        <div className="content">
-          <div className="form" method="post">
-            <div className="form__field">
-            <img src="../SVG/profiel.svg" class="icon" id="profileicon"> </img>
-            <input type="text" class="form-control" name="name" id="naam" placeholder="Gebruikersnaam"> </input>
-              
+          <div className="content">
+            <div className="form" method="post">
+              <div className="form__field">
+                <img src={profielsvg} className="icon" id="profileicon"/> 
+                <input type="text" className="form-control" name="name" id="naam" placeholder="Gebruikersnaam"></input>  
+              </div>
+              <div className="form__field">
+              <img src={passwordsvg} class="icon"/> 
+                <input type="password" className="form-control" name="password" id="password" placeholder="Wachtwoord"></input>
+              </div>
+              <div class="form__field">
+                <input type="submit" value="maak account" class="btn"></input>
+              </div>
             </div>
-            <div className="form__field">
-            <img src="../SVG/password.svg" class="icon" > </img>
-              <input type="password" class="form-control" name="password" id="password" placeholder="Wachtwoord"></input>
-            </div>
-          
-        
-        <div class="form__field">
-					<input type="submit" value="maak account" class="btn"> </input>
+            <a className="link-account" href="register.js">Heb je nog geen account?</a>
           </div>
-				</div>
-        <a class="link-account" href="register.js">Heb je nog geen account?</a>
-        </div>
       </div>
-
-
-     
-    );
+    </div>
+  );
 }
 
 export default Login;
