41b3a48783c588d3028f033ef63fc8d18162d832
jonasdebruyn <jonasdebruyn1@telenet.be>
2020-06-08 23:14:29 +0200

    achievements adjusting +settings

--- addication/src/routes/achievements.js
+++ addication/src/routes/achievements.js
@@ -1,6 +1,7 @@
 import React from 'react';
 //import './App.css';
 import '../css/stylejonas.css';
+import '../css/stylechloe.css';
 
 
 import award from "../SVG/award.svg";
@@ -8,6 +9,14 @@ import dagboek from "../SVG/dagboek.svg";
 import gestopt from "../SVG/gestopt.svg";
 import happy from "../SVG/happy.svg";
 import arrow from "../SVG/arrow.svg";
+import level3 from "../SVG/level3.svg";
+import level4 from "../SVG/level4.svg";
+import level5 from "../SVG/level5.svg";
+import level6 from "../SVG/level6.svg";
+import level7 from "../SVG/level7.svg";
+import level8 from "../SVG/level8.svg";
+import levelbalk from "../SVG/level-balk.svg";
+import levelaanduiding from "../SVG/level-aanduiding.svg";
 
 
 
@@ -18,6 +27,27 @@ function achievements() {
   return (
 
     <div id="scale" className="gradient">
+
+
+
+<header>
+          <div class="levelsheader"> 
+              <img src={level2} class="lev"/> 
+              <img src={level3} class="lev"/>
+              <img src={level4} class="lev"/>
+              <img src={level5} class="lev"/>
+              <img src={level6} class="lev"/>
+              <img src={level7} class="lev"/>
+              <img src={level8} class="lev"/>
+          </div>
+          <br />
+          <div class="currentlevel">
+            <img src={level1} class="level"/>
+            <img src={levelbalk} class="levelbalk"/>
+            <img src={levelaanduiding} class="levelaanduid"/>
+            <img src={levelaanduiding} class="levelaanduid1"/>
+          </div>
+        </header>
    
    <a href="#"><img class="arrow" src={arrow}></img></a>
 	<div class="level">
--- addication/src/routes/settings.js
+++ addication/src/routes/settings.js
@@ -1,8 +1,18 @@
 import React from 'react';
 //import './App.css';
 import '../css/stylejonas.css';
+import '../css/stylechloe.css';
 import pencilsvg from "../SVG/pencil.svg";
 import arrowsvg from "../SVG/arrow.svg";
+
+import level3 from "../SVG/level3.svg";
+import level4 from "../SVG/level4.svg";
+import level5 from "../SVG/level5.svg";
+import level6 from "../SVG/level6.svg";
+import level7 from "../SVG/level7.svg";
+import level8 from "../SVG/level8.svg";
+import levelbalk from "../SVG/level-balk.svg";
+import levelaanduiding from "../SVG/level-aanduiding.svg";
 
 
 
@@ -11,6 +21,25 @@ function settings() {
   return (
 
     <div id="scale" className="gradient">
+
+<header>
+           <div class="levelsheader"> 
+              <img src={level2} class="lev"/> 
+              <img src={level3} class="lev"/>
+              <img src={level4} class="lev"/>
+              <img src={level5} class="lev"/>
+              <img src={level6} class="lev"/>
+              <img src={level7} class="lev"/>
+              <img src={level8} class="lev"/>
+          </div>
+          <br />
+          <div class="currentlevel">
+            <img src={level1} class="level"/>
+            <img src={levelbalk} class="levelbalk"/>
+            <img src={levelaanduiding} class="levelaanduid"/>
+            <img src={levelaanduiding} class="levelaanduid1"/>
+          </div>
+        </header>
    
    
    
