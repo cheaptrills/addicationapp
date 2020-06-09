d75d6ffac4dd4d313c66873e25fc52057196a951
cheaptrills <vincevanalphen@yahoo.com>
2020-06-09 11:44:47 +0200

    started working on apollo

--- addication/src/index.js
+++ addication/src/index.js
@@ -7,6 +7,21 @@ import {GlobalProvider} from './context/GlobalContext';
 import { UserProvider } from './context/UserContext';
 import { DiaryProvider } from './context/DiaryContext';
 
+import ApolloClient from 'apollo-boost';
+import { gql } from "apollo-boost";
+
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+});
+
+client.query({
+    query: gql`{
+        rates(currency: "USD") {
+          currency
+        }
+      }`
+  }).then(result => console.log(result));
+
 ReactDOM.render(
     <DiaryProvider>
       <UserProvider>
