48efd1f399c94860b50612fdd2893a34ef7cf4ae
cheaptrills <vincevanalphen@yahoo.com>
2020-06-12 12:57:50 +0200

    created diary.js

--- addication/src/routes/Diary.js
+++ addication/src/routes/Diary.js
@@ -1,15 +1,53 @@
 import React, {useState, useContext} from 'react';
+import CurrentLevel from '../components/CurrentLevel';
+import backbutton from "../SVG/back-button.svg";
+
+import gql from 'graphql-tag';
+import {useQuery} from 'react-apollo';
 
 //import './App.css';
+const GET_DIARIES = gql
+`{
+  diaries( userID:"5ede2e9dc50f7022e0fad488" ) {
+    title
+  }
+}`;
 
 function Diary() {
 
-  return (
+  const{
+    loading, error, data 
+ }= useQuery(GET_DIARIES);
 
-    <div>
-        <p>
-        </p>
+ if(loading) return <h4>loading</h4>
+ if(error) console.log(error);
+ console.log(data);
+
+  return (
+  <div>
+    <header>
+      <div class="start"> 
+        <a href="dashboard.html"> 
+          <img src={backbutton} alt="terugknop" class="terugknop"/>
+        </a>
+      </div>
+      <br />
+      <CurrentLevel />
+    </header>
+    <div class="maindash"> 
+      {
+        data.diaries.map(diary => (
+          <a href="/diaryread">
+            <button class="dash" id="opdrachten"> 
+              <div class="inhoud">
+                <p class="dag">{diary.title}</p>
+              </div>
+            </button>
+          </a>
+          )) 
+        }
     </div>
+  </div>
   );
 
     
