4d289dcd7df826cbb6cb153192e10a79dd8fc737
cheaptrills <vincevanalphen@yahoo.com>
2020-06-13 13:55:32 +0200

    beautified Diaryentry

--- addication/src/routes/DiaryEntry.js
+++ addication/src/routes/DiaryEntry.js
@@ -1,5 +1,5 @@
-import React, {useState, useContext} from 'react';
-import {useHistory} from 'react-router-dom';
+import React, { useState, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 import CurrentLevel from '../components/CurrentLevel';
 import backbutton from "../SVG/back-button.svg";
 
@@ -18,21 +18,21 @@ function DiaryEntry() {
 
   let history = useHistory();
 
-  const handleEntry = ()=> {
-    if(fields.username == null < 2){
+  const handleEntry = () => {
+    if (fields.username == null < 2) {
       alert("Geef een titel in");
-      return null; 
+      return null;
     }
-    
-    if(fields.password == null || fields.password.length < 6){
+
+    if (fields.password == null || fields.password.length < 6) {
       alert("Vul je dagboek met wat meer woorden");
-      return null; 
+      return null;
     }
 
     history.push("/profile");
-  } 
+  }
 
-  const setField = ({target}, field) => {
+  const setField = ({ target }, field) => {
 
     let oldFields = fields;
     oldFields[field] = target.value;
@@ -43,55 +43,54 @@ function DiaryEntry() {
   return (
 
     <div>
-        <header>
-    <div class="start"> 
-       <a href="dashboard.html"> 
-        <img src={backbutton} alt="terugknop" class="terugknop"/>
-        </a>
-    </div>
-    
-     <br/>
-      <CurrentLevel />
-   </header>
-      
-      
-       <div class="maindash"> 
-       
-     <section class="dash1" id="opdrachten"> 
-     <div class="inhouddagboek">
-     
-      <h2 id="vulin"> Dag #</h2>
-      <form>
-      <textarea id="titeldagboek" name="titeldagboek" placeholder="Titel" rows="1" cols="10">
-      </textarea>  
-      <br /> 
-     <textarea id="textdagboek" name="tekstdagboek" placeholder="Vul het dagboek in" rows="10" cols="30">
-      </textarea>
-    <br/>
-   
-    <div class="gebruik"> 
-     <input type="checkbox" id="gebruikt" name="gebruikt" value="niet gebruikt"/> 
-          Ik heb vandaag niet gebruikt 
-          </div>
-         
-      <div class="form__field"> 
-                    
-					<input type="submit" value="OK" class="ok"/>
-				</div>
-      </form>
-      </div>
-     </section>
-      
-   
+      <header>
+        <div class="start">
+          <a href="dashboard.html">
+            <img src={backbutton} alt="terugknop" class="terugknop" />
+          </a>
+        </div>
+        <br />
+        <CurrentLevel />
+      </header>
 
-      
- 
-   </div>
+
+      <div class="maindash">
+
+        <section class="dash1" id="opdrachten">
+          <div class="inhouddagboek">
+
+            <h2 id="vulin"> Dag #</h2>
+            <form>
+              <textarea id="titeldagboek" name="titeldagboek" placeholder="Titel" rows="1" cols="10">
+              </textarea>
+              <br />
+              <textarea id="textdagboek" name="tekstdagboek" placeholder="Vul het dagboek in" rows="10" cols="30">
+              </textarea>
+              <br />
+
+              <div class="gebruik">
+                <input type="checkbox" id="gebruikt" name="gebruikt" value="niet gebruikt" />
+                 Ik heb vandaag niet gebruikt
+              </div>
+
+              <div class="form__field">
+
+                <input type="submit" value="OK" class="ok" />
+              </div>
+            </form>
+          </div>
+        </section>
+
+
+
+
+
+      </div>
 
     </div>
   );
 
-    
+
 }
 
 export default DiaryEntry;
