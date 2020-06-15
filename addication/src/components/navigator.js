dbb4e106614fe5597dce94c94b37ed83f6d5d3d7
cheaptrills <vincevanalphen@yahoo.com>
2020-06-15 15:21:34 +0200

    navigator links inorde gebracht

--- addication/src/components/navigator.js
+++ addication/src/components/navigator.js
@@ -6,29 +6,33 @@ import home from "../SVG/home.svg";
 import taken from "../SVG/takenlijst.svg";
 import dagboek from "../SVG/dagboek.svg";
 import profiel from "../SVG/profiel.svg";
+import { useHistory } from "react-router-dom";
 
-export class Navigator extends Component{
-    render() {
+
+export function Navigator() {
+
+    const history = useHistory();
+    
         return(
             <div>
                 <nav  class="nav">
                     <img class="navbox" src={navigator} />
-                    <a href="#">
+                    <a onClick={() => history.push("/home")}>
                         <img src={home} alt="" class="home"></img>
                     </a>
-                    <a href="#">
+                    <a onClick={() => history.push("/opdrachtkeuze")}>
                         <img class="takenlijst" src={taken} ></img>  
                     </a>
-                    <a href="#">
+                    <a onClick={() => history.push("/diarymenu")}>
                         <img class="dagboek" src={dagboek} ></img>
                     </a>
-                    <a href="#">
+                    <a onClick={() => history.push("/profile")}>
                         <img src={profiel} alt="" class="profiel"></img>
                     </a>
 	            </nav>
             </div>
         )
     }
-}
+
 
 export default Navigator;
